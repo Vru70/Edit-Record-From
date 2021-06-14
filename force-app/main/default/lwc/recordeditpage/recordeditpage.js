58e85d0a6c8c64d85dfb2d872a1ea7d712799895
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-06-14 16:04:04 +0530

    logs removed

--- force-app/main/default/lwc/recordeditpage/recordeditpage.js
+++ force-app/main/default/lwc/recordeditpage/recordeditpage.js
@@ -9,30 +9,27 @@ export default class Recordeditpage extends LightningElement
     @api SFDCobjectApiName; // collect Object API name
     @api fieldSetName; // set fieldset name 
 
-   // @track inputField = 'Name';
-    @track fieldnames='';
+    @track fieldnames;
 
     connectedCallback()
     {
-        console.log('this.SFDCobjectApiName - :', this.SFDCobjectApiName);
-        console.log('this.fieldSetName - :', this.fieldSetName);
         getSObjectRecords({
             SFDCobjectApiName: this.SFDCobjectApiName,
             fieldSetName: this.fieldSetName
         })
-        .then((data) => {
+            .then((data) => {
             
             this.fieldnames = data;
-            console.log('data=>:', JSON.stringify(data));
-
-            console.log('this.fieldnames=>:', JSON.stringify(this.fieldnames));
-            
         })
         .catch(error => {
             this.error = error;
             console.log('error', error);
         })
+    }
 
-        console.log('recordId:', this.recordId);
+    handleChange(event)
+    {
+
+
     }
 }
