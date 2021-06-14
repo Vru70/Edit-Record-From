9c4490a7e5ed6c0e6596d46c9850c626b93bbd50
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-06-14 08:16:52 +0530

    lwc comp

--- force-app/main/default/lwc/recordeditpage/recordeditpage.js
+++ force-app/main/default/lwc/recordeditpage/recordeditpage.js
@@ -1,3 +1,51 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement, track } from 'lwc';
+import getSObjectRecords from '@salesforce/apex/getRecordDataList.getSObjectRecords';
 
-export default class Recordeditpage extends LightningElement {}
+export default class Recordeditpage extends LightningElement {
+    @api recordId; // collectes record Id 
+
+    @api SFDCobjectApiName; // collect Object API name 
+    @api fieldSetName; // set fieldset name 
+
+    @track fieldsList;
+    @track inputField = 'Name';
+
+
+    connectedCallback()
+    {
+        getSObjectRecords({
+            strObjectApiName: this.SFDCobjectApiName,
+            strfieldSetName: this.fieldSetName
+        })
+            .then((data) => {
+            /*
+            //get the entire map
+            let objStr = JSON.parse(data);
+             retrieve listOfFields from the map,
+            here order is reverse of the way it has been inserted in the map 
+            let listOfFields = JSON.parse(Object.values(objStr)[1]);
+
+            let items = []; //local array to prepare columns
+
+            listOfFields.map(element =>
+            {
+                items = [...items, {
+                    fieldName: element.fieldPath
+                }];
+            });
+
+            this.columns = items;*/
+            //
+                this.fieldsList = data;
+                console.log('this.fieldsList:', JSON.stringify(this.fieldsList));
+            
+        })
+        .catch(error => {
+            this.error = error;
+            console.log('error', error);
+           
+        })
+
+        console.log('recordId:', recordId);
+    }
+}
