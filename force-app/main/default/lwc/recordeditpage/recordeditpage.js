e37e302f48f4aef7354e31bf97cafca0efff2bd1
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-06-14 14:19:49 +0530

    value passig issues

--- force-app/main/default/lwc/recordeditpage/recordeditpage.js
+++ force-app/main/default/lwc/recordeditpage/recordeditpage.js
@@ -1,51 +1,38 @@
 import { api, LightningElement, track } from 'lwc';
+
 import getSObjectRecords from '@salesforce/apex/getRecordDataList.getSObjectRecords';
 
-export default class Recordeditpage extends LightningElement {
+export default class Recordeditpage extends LightningElement
+{
     @api recordId; // collectes record Id 
 
-    @api SFDCobjectApiName; // collect Object API name 
+    @api SFDCobjectApiName; // collect Object API name
     @api fieldSetName; // set fieldset name 
 
-    @track fieldsList;
-    @track inputField = 'Name';
-
+   // @track inputField = 'Name';
+    @track fieldnames='';
 
     connectedCallback()
     {
+        console.log('this.SFDCobjectApiName - :', this.SFDCobjectApiName);
+        console.log('this.fieldSetName - :', this.fieldSetName);
         getSObjectRecords({
-            strObjectApiName: this.SFDCobjectApiName,
-            strfieldSetName: this.fieldSetName
+            SFDCobjectApiName: this.SFDCobjectApiName,
+            fieldSetName: this.fieldSetName
         })
-            .then((data) => {
-            /*
-            //get the entire map
-            let objStr = JSON.parse(data);
-             retrieve listOfFields from the map,
-            here order is reverse of the way it has been inserted in the map 
-            let listOfFields = JSON.parse(Object.values(objStr)[1]);
+        .then((data) => {
+            
+            this.fieldnames = data;
+            console.log('data=>:', JSON.stringify(data));
 
-            let items = []; //local array to prepare columns
-
-            listOfFields.map(element =>
-            {
-                items = [...items, {
-                    fieldName: element.fieldPath
-                }];
-            });
-
-            this.columns = items;*/
-            //
-                this.fieldsList = data;
-                console.log('this.fieldsList:', JSON.stringify(this.fieldsList));
+            console.log('this.fieldnames=>:', JSON.stringify(this.fieldnames));
             
         })
         .catch(error => {
             this.error = error;
             console.log('error', error);
-           
         })
 
-        console.log('recordId:', recordId);
+        console.log('recordId:', this.recordId);
     }
 }
