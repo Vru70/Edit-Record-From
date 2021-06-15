59204ccc31e4224275ea3c9e8c33235c4b3e9b1e
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-06-15 11:52:43 +0530

    toast event and handleFocusOut

--- force-app/main/default/lwc/recordeditpage/recordeditpage.js
+++ force-app/main/default/lwc/recordeditpage/recordeditpage.js
@@ -1,6 +1,8 @@
 import { api, LightningElement, track } from 'lwc';
 
 import getSObjectRecords from '@salesforce/apex/getRecordDataList.getSObjectRecords';
+import setSObjectRecords from '@salesforce/apex/getRecordDataList.setSObjectRecords';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class Recordeditpage extends LightningElement
 {
@@ -10,6 +12,12 @@ export default class Recordeditpage extends LightningElement
     @api fieldSetName; // set fieldset name 
 
     @track fieldnames;
+    @track flagChanged = false;
+    error;
+    // Tost variable
+    variant = '';
+    title = '';
+    message = '';
 
     connectedCallback()
     {
@@ -18,18 +26,74 @@ export default class Recordeditpage extends LightningElement
             fieldSetName: this.fieldSetName
         })
             .then((data) => {
-            
-            this.fieldnames = data;
-        })
-        .catch(error => {
-            this.error = error;
-            console.log('error', error);
-        })
+
+                this.fieldnames = data;
+                this.error = undefined;
+            })
+            .catch(error => {
+                this.error = error;
+                this.fieldnames = undefined;
+                this.variant = 'error';
+                this.message = '' + error;
+                this.title = 'Error in Loading SObject Record';
+                showToast();
+                console.log('error in loading', error);
+            })
     }
 
-    handleChange(event)
+    handleChnage(event)
     {
+        // console.log('Changed : ', JSON.stringify(event.target.fieldName));
+        this.flagChanged = true;
+    }
 
+    handleFocusOut(event)
+    {
+        if (this.flagChanged)
+        {
+            this.flagChanged = false;
+            //console.log('fieldName :' + JSON.stringify(event.target.fieldName) + ' Value: ' + JSON.stringify(event.target.value));
+            setSObjectRecords({
+                fieldName: event.target.fieldName,
+                fieldValue: event.target.value,
+                recordId: this.recordId,
+                SFDCobjectApiName: this.SFDCobjectApiName
+            })
+            .then((data) => {
+                console.log('data aftr saving : ', data);
+                if (data === 'Success')
+                {
+                    this.variant = 'success';
+                    this.message = 'Saved Successfully';
+                    this.title = 'Saved Successfully';
+                    showToast();
+                } else
+                {
+                    this.variant = 'error';
+                    this.message = '' + data;
+                    this.title = 'Error in Saving SObject Record';
+                    showToast();                        
+                }
+                
+            })
+            .catch(error => {
+                this.variant = 'error';
+                this.message = '' + error;
+                this.title = 'Error in Saving SObject Record';
+                showToast();
+                console.log('error in saving', error);
+            })
+        }
+    }
 
+    showToast()
+    {
+        const evt = new ShowToastEvent({
+            title: this.title,
+            message: this.message,
+            variant: this.variant,
+            mode: 'dismissable'
+        });
+        this.dispatchEvent(evt);
     }
 }
