178164b19e43f88730732cda219297ad4259e33e
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-07-02 00:50:57 +0530

    timeout added

--- force-app/main/default/lwc/searchReplace_datatable/searchReplace_datatable.js
+++ force-app/main/default/lwc/searchReplace_datatable/searchReplace_datatable.js
@@ -7,32 +7,29 @@
  * 1.0   06-25-2021   Vrushabh Uprikar   Initial Version
 **/
 import { LightningElement, wire, api, track } from 'lwc';
-import { refreshApex } from '@salesforce/apex';
+import { getRecordNotifyChange } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getFieldsAndRecords from '@salesforce/apex/SearchReplaceController.getFieldsAndRecords';
 import updateRecords from '@salesforce/apex/SearchReplaceController.updateRecords';
 
 export default class SearchReplace_datatable extends LightningElement {
 
-    @track error;
+    error;
     @track searchKey = '';
     @track replacetext = [];
 
     @track allData = []; // Datatable
-    @track allDataOrgCopy = []; // DatatableOrignalCpy
+    allDataOrgCopy = []; // DatatableOrignalCpy
     @track columns;
 
     @track fieldOption = '';
     @track fieldOptionJSON; // list of fields option for combobox
     @track FieldsValue; // default value of combobox
 
-
     @api SFDCobjectApiName;
     @api fieldSetName;
 
-
-    @track isSearchFlag = false;
-    @track getUpdateRecData = '';
+    isSearchFlag = false;
 
     connectedCallback() {
         getFieldsAndRecords({
@@ -86,39 +83,58 @@ export default class SearchReplace_datatable extends LightningElement {
     }
 
     handleReplace() { // onClick Replace
-        console.log('replace text:', this.replacetext);
-        if (this.isSearchFlag && (this.replacetext != '' || this.replacetext == null)) {
 
+        if (this.isSearchFlag && (this.replacetext != '' || this.replacetext == null)) {
             this.isSearchFlag = false;
             let data_to_replace = this.allData;
+
             data_to_replace.forEach(element => {
-                this.getUpdateRecData = updateRecords({
+                updateRecords({
                     strObjectApiName: this.SFDCobjectApiName,
                     Record_Id: element.Id,
                     Replace_text: this.replacetext,
                     Replace_field: this.fieldOption
-                });
-            });
-            console.log('getUpdateRecData:', this.getUpdateRecData);
-            if (this.getUpdateRecData == 'Success') {
-                const evt = new ShowToastEvent({
-                    title: 'Success',
-                    message: 'Rename sucessful',
-                    variant: 'success',
-                    mode: 'dismissable'
-                });
-                this.dispatchEvent(evt);
-            } else {
+                })
+                    .then(result => {
+                        console.log('result:', result);
+                        if (result == 'Success') {
+                            const evt = new ShowToastEvent({
+                                title: 'Success',
+                                message: 'Rename Sucessful',
+                                variant: 'success',
+                                mode: 'dismissable'
+                            });
+                            this.dispatchEvent(evt);
+                        } else {
+                            const evt = new ShowToastEvent({
+                                title: 'Error',
+                                message: result,
+                                variant: 'error',
+                                mode: 'dismissable'
+                            });
+                            this.dispatchEvent(evt);
+                        }
+                    })
+                    .catch((error) => {
+                        console.log('error:', error);
+                        const evt = new ShowToastEvent({
+                            title: 'Error',
+                            message: error,
+                            variant: 'error',
+                            mode: 'dismissable'
+                        });
+                        this.dispatchEvent(evt);
+                    });
 
-                const evt = new ShowToastEvent({
-                    title: 'Error',
-                    message: this.getUpdateRecData,
-                    variant: 'error',
-                    mode: 'dismissable'
-                });
-                this.dispatchEvent(evt);
-            }
-        } else {
+                getRecordNotifyChange([{ recordId: element.Id }]);
+            });
+
+            setTimeout(() => {
+                this.connectedCallback();
+            }, 1900);
+
+        }
+        else {
             let evt = new ShowToastEvent({
                 title: 'Warning',
                 message: 'Replace text should not be empty',
@@ -127,22 +143,22 @@ export default class SearchReplace_datatable extends LightningElement {
             });
             this.dispatchEvent(evt);
         }
+
     }
 
     searchDataTable() {
         var searchString = this.searchKey
-        var allRecords = this.allData;
+        var allRecords = this.allDataOrgCopy;
         var searchResults = [];
-        searchResults = allRecords.filter(key => key[this.fieldOption] == searchString);
+        searchResults = allRecords.filter(key => key[this.fieldOption].includes(searchString));
         this.allData = searchResults;
-
     }
 
     handleKeyChange(event) {
 
         this.searchKey = event.target.value;
 
-        if (this.searchKey != null) {
+        if (this.searchKey !== null) {
             this.isSearchFlag = true;
             this.searchDataTable();
         }
@@ -154,7 +170,6 @@ export default class SearchReplace_datatable extends LightningElement {
     get FieldsOptions() { // combobox option set
         return this.fieldOptionJSON;
     }
-
 
     handleChangeFields(event) { // on chnage of combobox value
         this.fieldOption = event.target.value;
